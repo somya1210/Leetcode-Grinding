252373fee058884c3ee0aaabc44e23e75c34ca46
somya1210 <52173052+somya1210@users.noreply.github.com>
2021-06-18 15:53:06 +0530

    Update binary-tree-maximum-path-sum.cpp

--- binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -14,12 +14,12 @@ public:
     int find(TreeNode* root,int &ans)
     {
         if(!root)
-             return 0;
-        int l_sum=find(root->left,ans);
+             return 0;  // base condition
+        int l_sum=find(root->left,ans);  // hypothesis
         int r_sum=find(root->right,ans);
-        int temp=max(max(l_sum,r_sum)+root->val,root->val); // pass nahi ho ra h us node s
-        int res=max(temp,l_sum+r_sum+root->val);
-        ans=max(max(ans,res),root->val);
+        int temp=max(max(l_sum,r_sum)+root->val,root->val); // induction -> not passing through that node , either the current node is max or the sum from its left or right child
+        int res=max(temp,l_sum+r_sum+root->val);  // update the answer each time
+        ans=max(ans,res);
         return temp;
     }
     int maxPathSum(TreeNode* root) {
@@ -27,4 +27,5 @@ public:
         find(root,ans);
         return ans;
     }
+    // This problem is just like to find diameter of a binary tree with the edge case that if the left and right child of any node return negative values than we have to return the root->value 
 };
