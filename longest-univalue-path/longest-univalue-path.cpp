3b2baf448b338fcd10063276b135381a07e46f72
somya1210 <52173052+somya1210@users.noreply.github.com>
2021-06-18 14:20:31 +0530

    Update longest-univalue-path.cpp

--- longest-univalue-path/longest-univalue-path.cpp
+++ longest-univalue-path/longest-univalue-path.cpp
@@ -11,18 +11,14 @@
  */
 class Solution {
 public:
-    int find(TreeNode* root,int &ans)
+     int find(TreeNode* root,int &ans)
     {
-        if(root)
-        {int l,r;
-            if(root->left)
-             l=find(root->left,ans);
-            else
-                l=0;
-            if(root->right)
-                r=find(root->right,ans);
-            else
-                r=0;
+        if(!root)
+             return 0;
+        int l,r;
+        l=find(root->left,ans);
+        r=find(root->right,ans);
+           
            if(root->left&&root->left->val==root->val)
                l++;
            else
@@ -35,9 +31,9 @@ public:
                
            ans=max(ans,l+r);
            return max(l,r);
-                
-        }
-        return 0;
+        // l ko zero karna and r ko zero karna ek essential step h kyunki kisi bhi node p hokar 
+        // agar uska left ya right uske equal hi nhi h to kya matalb h find karne ka 
+        // and in case if the root value is equal to both of its left and right child we will update //l and r value so that at the end we obtain the max value
     }
     int longestUnivaluePath(TreeNode* root) {
         int ans=0;
