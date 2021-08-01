a14f1c156306cdd2d3e538503b15fb13e971d2dc
somya1210 <52173052+somya1210@users.noreply.github.com>
2021-08-01 14:59:20 +0530

    Time: 4 ms (79.68%), Space: 7.5 MB (88.46%) - LeetHub

---
+++ stone-game/stone-game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // the no of piles are even so har time p ya to even chose kare ya odd alice k uoar h
+      return 1;
+    }
+};
