c1636dfa1fd9714decbb2a3e935889cc459e2a48
somya1210 <52173052+somya1210@users.noreply.github.com>
2021-08-15 07:45:41 +0530

    Time: 11 ms (20.65%), Space: 13.1 MB (51.76%) - LeetHub

---
+++ rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    bool inbound(int i,int j,int n,int m)
+    {
+        if(i<0||j<0||i>=n||j>=m)
+            return false;
+        return true;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+     int time=0;
+        queue<pair<int,int>>q;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]==2)
+                    q.push({i,j});
+            }
+        }
+        int n=grid.size(),m=grid[0].size();
+        while(!q.empty())
+        {
+            int no=q.size(); bool first =true;
+            while(no--)
+            {
+                int x=q.front().first;
+                int y=q.front().second; 
+                q.pop();
+                if(inbound(x-1,y,n,m)&&grid[x-1][y]==1)
+                {
+                    if(first)
+                    { time++;
+                    first=false;}
+                    q.push({x-1,y});
+                    grid[x-1][y]=2;
+                }
+                if(inbound(x+1,y,n,m)&&grid[x+1][y]==1)
+                {
+                    if(first)
+                    { time++;
+                    first=false;}
+                    q.push({x+1,y});
+                    grid[x+1][y]=2;
+                }
+                if(inbound(x,y-1,n,m)&&grid[x][y-1]==1)
+                {
+                    if(first)
+                    {time++;
+                    first=false;}
+                    q.push({x,y-1});
+                    grid[x][y-1]=2;
+                }
+                if(inbound(x,y+1,n,m)&&grid[x][y+1]==1)
+                {
+                    if(first)
+                    {time++;
+                    first=false;}
+                    q.push({x,y+1});
+                    grid[x][y+1]=2;
+                }
+            }
+        }
+         for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]==1)
+                    return -1;
+            }
+        }
+        return time;
+    }
+};
