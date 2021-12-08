e03c968c2e8fe2523305b8465c7adfb7f7bf1087
somya1210 <52173052+somya1210@users.noreply.github.com>
2021-12-08 21:56:03 +0530

    Time: 6 ms (6.53%), Space: 7.2 MB (31.64%) - LeetHub

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& mat) {
-            if(mat.size()!=1)
+       // transpose(mat);
+        int n=mat.size();
+        for(int i=0;i<n;i++)
         {
-            int n = mat.size();
-            for(int i=0;i<(n/2);i++)
+            for(int j=i;j<n;j++) // j=i kyunki agr nhi kie toh gadbad 
             {
-                for(int j=i;j<n-i-1;j++)
-                {
-                    int tmp = mat[i][j];
-                    mat[i][j] = mat[n-j-1][i];
-                    mat[n-j-1][i] = mat[n-i-1][n-j-1];
-                    mat[n-i-1][n-j-1] = mat[j][n-i-1];
-                    mat[j][n-i-1] = tmp;
-                }
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n/2;j++)
+            {
+                int temp=mat[i][j];
+                mat[i][j]=mat[i][n-1-j];
+                mat[i][n-1-j]=temp;
             }
         }
     }
