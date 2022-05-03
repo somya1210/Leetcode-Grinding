75788cd4e4373303f39334fa87a4acd3fb87d3d0
somya1210 <52173052+somya1210@users.noreply.github.com>
2022-05-03 13:48:23 +0530

    Added solution - LeetHub

---
+++ Subarrays with equal 1s and 0s - GFG/subarrays-with-equal-1s-and-0s.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution{
+  public:
+    //Function to count subarrays with 1s and 0s.
+    long long int countSubarrWithEqualZeroAndOne(int arr[], int n)
+    {
+        //Your code here
+        long long int ans=0;
+        map<int,int>map;
+        for(int i=0;i<n;i++)
+        if(arr[i]==0)
+        arr[i]=-1;
+        map[0]=1;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        { 
+            sum+=arr[i];
+            if(map.find(sum)!=map.end())
+            {
+                ans+=map[sum];
+                map[sum]+=1;
+            }
+            else
+            {
+                map[sum]=1;
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int n,i;
+	cin>>n;
+	int arr[n];
+	for(i=0;i<n;i++)
+	cin>>arr[i];
+	Solution obj;
+	cout<< obj.countSubarrWithEqualZeroAndOne(arr, n)<<"\n";
+	}
+	return 0;
+}
+  // } Driver Code Ends
