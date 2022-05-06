2f01b3bc7ec6be46dbd49aceb60850cff40c33a5
somya1210 <52173052+somya1210@users.noreply.github.com>
2022-05-06 22:13:49 +0530

    Time: 11 ms (87.66%), Space: 11.3 MB (17.10%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,13 +2,25 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // brute force approach o(n2)
-        for(int i=0;i<nums.size();i++)
+      /*  for(int i=0;i<nums.size();i++)
         {
             for(int j=i+1;j<nums.size();j++)
             {
                 if(nums[i]+nums[j]==target)
                     return {i,j};
             }
+        }
+        return {};
+        */
+        map<int,int>map;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(map.find(target-nums[i])!=map.end())
+            {
+                return {i,map[target-nums[i]]};
+            }
+            else
+                map[nums[i]]=i;
         }
         return {};
     }
