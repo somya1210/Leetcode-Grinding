1a44d78c225ca323d8662470b3f22444aff7818f
somya1210 <52173052+somya1210@users.noreply.github.com>
2022-05-17 17:28:41 +0530

    Time: 4 ms (90.39%), Space: 8.8 MB (50.87%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>>dp=triangle;
-        int n=triangle.size();
-        for(int i=n-2;i>=0;i--)
-        {
-            for(int j=0;j<triangle[i].size();j++)
-            {
-                dp[i][j]+=min(dp[i+1][j],dp[i+1][j+1]);
-            }
-        }
-        return dp[0][0];
+    int dp[201][201];//={-1};
+    int minimumTotal(vector<vector<int>>&t) {
+        memset(dp,-1,sizeof(dp));
+   //     int n=triangle.size();//,m=triangle[0].size()
+    return find(0,0,t);
     }
+    int find(int i,int j,vector<vector<int>>&t)
+    {
+        if(i==t.size()-1)
+            return t[i][j];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int findone=find(i+1,j,t);
+        int findtwo=find(i+1,j+1,t);
+        return dp[i][j]=min(findone,findtwo)+t[i][j];
+    }
+    
 };
