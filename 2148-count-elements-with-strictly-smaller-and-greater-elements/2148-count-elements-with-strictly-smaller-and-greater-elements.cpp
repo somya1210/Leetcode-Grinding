f5f0aedbf021a90b2f7c8b38b2baca050af25e22
somya1210 <52173052+somya1210@users.noreply.github.com>
2022-05-24 16:33:05 +0530

    Time: 7 ms (41.60%), Space: 8.9 MB (97.48%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -8,22 +8,29 @@ public:
         // 1 2 7 7 11 12 13 13
         // 2+1
         //8-3
-        int maxno=INT_MIN,minno=INT_MAX;
-        int maxfreq=0,minfreq=0;
-        for(int i=0;i<nums.size();i++)
+        int maxno=nums[0],minno=nums[0];
+        int maxfreq=1,minfreq=1;
+        for(int i=1;i<nums.size();i++)
         {
             if(nums[i]>maxno)
-                maxno=nums[i];
+            {maxno=nums[i];
+             maxfreq=1;
+            }
+            else
+                if(nums[i]==maxno)
+                {
+                    maxfreq++;
+                }
             if(nums[i]<minno)
-                minno=nums[i];
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==maxno)
-                maxfreq++;
-            if(nums[i]==minno)
+            {  minno=nums[i];
+             minfreq=1;
+            }
+            else if(nums[i]==minno)
+            {
                 minfreq++;
+            }
         }
+      
         // 1 -(1+1)->1-2=-1
         cout<<minfreq<<""<<maxfreq<<endl;
         int find=nums.size()-minfreq-maxfreq;
